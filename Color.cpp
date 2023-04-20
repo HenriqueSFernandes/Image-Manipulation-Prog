d261a3ff5938519fcbe62e7811f847ecd3a8009e
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-04-20 11:25:40 +0100

    finished color functions

--- Color.cpp
+++ Color.cpp
@@ -4,39 +4,52 @@ namespace prog
 {
     Color::Color()
     {
+        this->red_ = 0;
+        this->green_ = 0;
+        this->blue_ = 0;
     }
+    
     Color::Color(const Color &other)
     {
-    }
-    Color::Color(rgb_value red, rgb_value green, rgb_value blue)
-    {
-    }
-    rgb_value Color::red() const
-    {
-        return 0;
-    }
-    rgb_value Color::green() const
-    {
-        return 0;
-    }
-    rgb_value Color::blue() const
-    {
-        return 0;
+        this->red_ = other.red_;
+        this->green_ = other.green_;
+        this->blue_ = other.blue_;
     }
 
-    // TODO: remove this DUMMY variable once you have appropriate fields for representing colors.
-    rgb_value DUMMY_rgb_value = 0;
+    Color::Color(rgb_value red, rgb_value green, rgb_value blue)
+    {
+        this->red_ = red;
+        this->green_ = green;
+        this->blue_ = blue;
+    }
+
+    rgb_value Color::red() const
+    {
+        return this->red_;
+    }
+
+    rgb_value Color::green() const
+    {
+        return this->green_;
+    }
+
+    rgb_value Color::blue() const
+    {
+        return this->blue_;
+    }
 
     rgb_value &Color::red()
     {
-        return DUMMY_rgb_value;
+        return this->red_;
     }
+
     rgb_value &Color::green()
     {
-        return DUMMY_rgb_value;
+        return this->green_;
     }
+
     rgb_value &Color::blue()
     {
-        return DUMMY_rgb_value;
+        return this->blue_;
     }
 }
