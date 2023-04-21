ce5ea26ea855d5faf621f34dd47677b0bad60de4
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-04-21 19:14:27 +0100

    WIP: Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -4,29 +4,43 @@ namespace prog
 {
   Image::Image(int w, int h, const Color &fill)
   {
-  }
-  Image::~Image()
-  {
-  }
-  int Image::width() const
-  {
-    return -1;
-  }
-  int Image::height() const
-  {
-    return -1;
+    this->img = new Color *[h];
+    for (int i = 0; i < h; i++)
+    {
+      this->img[i] = new Color[w];
+      for (int j = 0; j < w; j++)
+      {
+        this->img[i][j] = fill;
+      }
+    }
   }
 
-  // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
-  Color DUMMY_color;
+  Image::~Image()
+  {
+    for (int i = 0; i < this->height_; i++)
+    {
+      delete[] this->img[i];
+    }
+    delete[] this->img;
+  }
+
+  int Image::width() const
+  {
+    return this->width_;
+  }
+
+  int Image::height() const
+  {
+    return this->height_;
+  }
 
   Color &Image::at(int x, int y)
   {
-    return DUMMY_color;
+    return this->img[x][y];
   }
 
   const Color &Image::at(int x, int y) const
   {
-    return DUMMY_color;
+    return this->img[x][y];
   }
 }
